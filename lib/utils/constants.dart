06f17b26d78ccd3da8f44c908b589f75bfb95f34
Clevrem <clevrem2@gmail.com>
2025-01-03 16:54:13 +0500

    hammasi o'z o'rniga joylashdi

---
+++ lib/utils/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color backroundColor = Color(0xFF1C0F0D);
+  static const Color redPIlltxtmain = Color(0xFF1C0F0D);
+  static const Color iconCOncolor = Color(0xFFFD5D69);
+  static const Color iconCOncolor2 = Color(0xFFEC888D);
+}
